9ce25edad46ca27901d54a778be755e86ff95d53
onomorph <onomorph@gmail.com>
2023-08-04 02:32:52 -0400

    2do

---
+++ Sesion 5/Practica/djangogirls/relaciones/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+
+class Automotriz(models.Model):
+    nombre = models.CharField(max_length=255)
+    
+    
+    def __str__(self):
+        return self.nombre
+
+class TipoCombustible(models.Model):
+    nombre = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.nombre
+    
+class ModeloCarro(models.Model):
+    nombre = models.CharField(max_length=255)
+    automotriz = models.ForeignKey(Automotriz, on_delete=models.SET_NULL, blank=True, null=True)
+    tipo_combustible = models.ManyToManyField(TipoCombustible)
+    
+    def __str__(self):
+        return self.nombre
